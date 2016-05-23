c4263750e3da13ea7ecfdc89c7adc47ab71852c2
Hans Miikael Uuspõld <hans.miikael.uuspold@icefire.ee>
2016-05-23 15:24:11 +0300

    Added nested routes.

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
-
+import PostsIndex from './components/posts_index';
+//Keep the App as the parent container so it'd be easy to add footers and headers and the like.
+//PostsIndex is nested in App, in order to render it, have to call out this.props.children in App.
 export default (
-    <Route path="/" component={App} />
+    <Route path="/" component={App}>
+        <IndexRoute component={PostsIndex} />
+    </Route>
 );
